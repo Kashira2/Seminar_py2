393e4b5b2a5fdf6647bd34b7885c4b82b473307c
Kashira2 <nekit.egorov@icloud.com>
2022-12-22 05:26:20 +0300

    dz3 commit

--- dz3.py
+++ dz3.py
@@ -6,4 +6,21 @@
 
 from random import randint as RI
 
-print(RI(0,100))
+num1 = int(input('Задайте размер списка: '))
+my_list = []
+
+for i in range(num1):
+    my_list.append(RI(0,100))
+print(my_list)
+
+for i in range(0, num1, 2):
+    if my_list[i] != my_list[len(my_list)-1]:    
+        temp = my_list[i]
+        my_list[i] = my_list[i+1]
+        my_list[i+1] = temp
+    else:
+        temp = my_list[i]
+        my_list[i] = my_list[0]
+        my_list[0] = temp
+
+print(my_list)
